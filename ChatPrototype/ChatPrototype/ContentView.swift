61315c02560562f9e9c6756c8341ad36da825b09
Amogh Ghadge <amoghghadge22@gmail.com>
2026-02-21 16:24:00 -0500

    views and modifiers

--- ChatPrototype/ChatPrototype/ContentView.swift
+++ ChatPrototype/ChatPrototype/ContentView.swift
@@ -10,10 +10,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
             Text("Knock, knock!")
+                .padding()
+                .background(Color.yellow, in: RoundedRectangle(cornerRadius: 8))
+            Text("Who's There?")
+                .padding()
+                .background(Color.teal, in: RoundedRectangle(cornerRadius: 8))
+                .padding()
+                .background(Color.red, in: Rectangle())
         }
         .padding()
     }
