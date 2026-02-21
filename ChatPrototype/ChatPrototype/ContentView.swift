d58f8a71893a089b50a63381394958caa0523ccc
Amogh Ghadge <amoghghadge22@gmail.com>
2026-02-21 16:51:58 -0500

    more tinkering with modifiers

--- ChatPrototype/ChatPrototype/ContentView.swift
+++ ChatPrototype/ChatPrototype/ContentView.swift
@@ -9,13 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        VStack(spacing: 40) {
             Text("Knock, knock!")
+                .foregroundStyle(Color.blue)
                 .padding()
+//                .shadow(color: .black, radius: 13)
                 .background(Color.yellow, in: RoundedRectangle(cornerRadius: 8))
+                .compositingGroup()
+                .shadow(color: .black, radius: 13)
             Text("Who's There?")
                 .padding()
                 .background(Color.teal, in: RoundedRectangle(cornerRadius: 8))
+                .shadow(color: .black, radius: 10)
                 .padding()
                 .background(Color.red, in: Rectangle())
         }
