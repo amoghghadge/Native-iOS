77a8469b2967f9adb6e3e71706a89f3512e2fd22
Amogh Ghadge <amoghghadge22@gmail.com>
2026-02-23 23:45:18 -0500

    dice roller

---
+++ DiceRoller/DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Amogh Ghadge on 2/23/26.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
